b90b2a923418b6b39bf5bbb9cfa898b0b98f21b7
Von VanMany <vonvanmany@gmail.com>
2024-03-20 19:20:47 -0400

    printing number

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,13 @@
-# add your code here
+# FizzBuzz printing numbers 1 to 100
+
+for num in range (101):
+   # print(num)
+
+#number is dividable by 3 print Fizz
+    
+    if num % 3 == 0:
+        print("Fizz")
+    else:
+        print(num)
+
 
